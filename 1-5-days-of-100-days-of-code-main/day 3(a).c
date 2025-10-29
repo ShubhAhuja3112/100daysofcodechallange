128852c5045505855212c9003fb32cd88f5a1aec
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-10-29 16:45:30 +0530

    Update day 3(a).c

--- 1-5-days-of-100-days-of-code-main/day 3(a).c
+++ 1-5-days-of-100-days-of-code-main/day 3(a).c
@@ -3,7 +3,7 @@
 int main() 
 {
     float celsius, fahrenheit;
-
+    printf("Name Shubh\nSAP ID -590021265\nCourse-BCA\nBatch-B5\n");
     printf("Enter temperature in Celsius\n ");
     scanf("%f",&celsius);
     fahrenheit=(celsius * 9 / 5) + 32;
