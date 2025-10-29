f3b616b06daa262098f6e193d837f978f39937b1
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-10-29 16:56:52 +0530

    Update day 20(b).c

--- 16-20-days-of-100-days-of-code-main/day 20(b).c
+++ 16-20-days-of-100-days-of-code-main/day 20(b).c
@@ -5,7 +5,7 @@ int main()
     int n ,last,product=1;
     printf("Enter n \n");
     scanf("%d",&n);
-    
+    printf("Name Shubh\nSAP ID -590021265\nCourse-BCA\nBatch-B5\n");    
     while(n!=0)
     {
         last=n%10;
