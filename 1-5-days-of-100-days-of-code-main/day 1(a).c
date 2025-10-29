3ba089e64a0c1c1d4747c3bb0975e5187b768d10
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-10-29 16:43:20 +0530

    Update day 1(a).c

--- 1-5-days-of-100-days-of-code-main/day 1(a).c
+++ 1-5-days-of-100-days-of-code-main/day 1(a).c
@@ -2,9 +2,10 @@
 
 int main() {
     int a,b,sum;
+    printf("Name Shubh\nSAP ID -590021265\nCourse-BCA\nBatch-B5\n");
     printf("Enter first number\n");
     scanf("%d",&a);
-
+    
     printf("Enter second number\n");
     scanf("%d",&b);
     sum=a+b;
