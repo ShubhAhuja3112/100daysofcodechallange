3be4a76c31168e95b8756e3b97d416092bb7aa12
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-10-29 16:53:36 +0530

    Add user info print statement
    Added user information print statement before factorial calculation.

--- 10-15-days-of-100-days-of-code-main/day 15(a).c
+++ 10-15-days-of-100-days-of-code-main/day 15(a).c
@@ -4,6 +4,7 @@ int main() {
 
 int n;
 int product =1;
+printf("Name Shubh\nSAP ID -590021265\nCourse-BCA\nBatch-B5\n");
 printf("Enter the number whose factorial is to be calculated  \n");
 scanf("%d",&n);
 
