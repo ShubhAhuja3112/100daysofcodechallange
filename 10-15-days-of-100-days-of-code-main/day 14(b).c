fd01128d014497b35f6837a080ea9774c0e036f9
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-10-29 16:53:18 +0530

    Print personal details at program start
    Added personal information output to the program.

--- 10-15-days-of-100-days-of-code-main/day 14(b).c
+++ 10-15-days-of-100-days-of-code-main/day 14(b).c
@@ -3,6 +3,7 @@
 int main() {
     int n;
     int sum = 0,count = 0;
+    printf("Name Shubh\nSAP ID -590021265\nCourse-BCA\nBatch-B5\n");
     int i=1;
     printf("Enter n\n ");
     scanf("%d",&n);
